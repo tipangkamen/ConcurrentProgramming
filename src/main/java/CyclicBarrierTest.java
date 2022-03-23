e7cd4bc912403243471370ecb949551f1d620d5d
zhuguancheng <1005071600@qq.com>
2022-03-24 00:10:28 +0800

    ...

--- src/main/java/CyclicBarrierTest.java
+++ src/main/java/CyclicBarrierTest.java
@@ -5,14 +5,14 @@ public class CyclicBarrierTest {
     public static void main(String[] args) {
         CyclicBarrier barrier = new CyclicBarrier(2, () -> System.out.println("发车"));
         for (int i = 0; i < 2; i++) {
-           new Thread(()->{
-               try {
-                   int await = barrier.await();
-                   System.out.println("await"+Thread.currentThread().getName()+"="+await);
-               } catch (InterruptedException | BrokenBarrierException e) {
-                   e.printStackTrace();
-               }
-           }).start();
+            new Thread(()->{
+                try {
+                    int await = barrier.await();
+                    System.out.println("await"+Thread.currentThread().getName()+"="+await);
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }).start();
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
