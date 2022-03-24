ff2454177760e56e23e22eb858740f20f08b0750
zhuguancheng <1005071600@qq.com>
2022-03-24 23:44:37 +0800

    <feat>:新增LockSupportTest

--- src/main/java/LockSupportTest.java
+++ src/main/java/LockSupportTest.java
@@ -4,7 +4,7 @@ import java.util.concurrent.locks.LockSupport;
 public class LockSupportTest {
     /*
      *
-     * LockSupport 的unpark可以先执行，之后再调用一次park也不会停止，但是如果调用多次就会停止 ,它应该是维护了一个park的数量
+     * LockSupport 的 unpark 可以先执行，之后再调用一次park也不会停止，但是如果调用多次就会停止 ,它应该是维护了一个park的数量
      *
      * */
     public static void main(String[] args) {
