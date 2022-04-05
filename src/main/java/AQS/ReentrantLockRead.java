d7ca3a18e3c86af37ce226fc57ea054c4ff5bc25
zhuguancheng <1005071600@qq.com>
2022-04-06 00:18:23 +0800

    <feat>:。。。

--- src/main/java/AQS/ReentrantLockRead.java
+++ src/main/java/AQS/ReentrantLockRead.java
@@ -1,13 +1,24 @@
 package AQS;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ReentrantLockRead {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         ReentrantLock reentrantLock = new ReentrantLock();
-        reentrantLock.lock();
-        reentrantLock.lock();
+
+        Thread thread1 = new Thread(() -> {
+            reentrantLock.lock();
+            reentrantLock.unlock();
+        }
+        );
+        thread1.start();
+        TimeUnit.SECONDS.sleep(1);
+
+        Thread thread2 = new Thread(() -> reentrantLock.lock());
+        thread2.start();
+
 
 
     }
---
+++ src/main/java/ThreadLocalTest.java
@@ -0,0 +1,6 @@
+public class ThreadLocalTest {
+    public static void main(String[] args) {
+        ThreadLocal<Phaser.Person> personThreadLocal=new ThreadLocal<>();
+       // personThreadLocal.set();
+    }
+}
